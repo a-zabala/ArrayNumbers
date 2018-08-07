8fadf6378725d9797a149218ccdcfa9842e25048
Angelina Zabala <angekr@aol.com>
2018-08-06 23:44:00 -0500

    just practice initializing an array and printing values

--- ArrayNumbers/Program.cs
+++ ArrayNumbers/Program.cs
@@ -6,7 +6,14 @@ namespace ArrayNumbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //initializes the array and assigns values
+            int[] numbers = new int[] { 1, 1, 2, 3, 5, 8 };
+            //this will iterate through the values of the array and print each to screen
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+            Console.ReadLine();
         }
     }
 }
